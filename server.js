0724bc1b3e820f0e56e393066c58fc963ce40a8f
dilixi <1284513393@qq.com>
2024-09-19 19:19:34 +0800

    修改一下

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname)));
 
 // 路由：默认为 index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index_bak.html'));
 });
 
 // 处理 YML 和 PNG 文件上传
